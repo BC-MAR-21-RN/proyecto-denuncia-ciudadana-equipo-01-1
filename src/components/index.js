389f0a511606a00eeb96f6b61cb39fdaa92d7fb4
Coreandrum1 <47857455+Coreandrum1@users.noreply.github.com>
2021-05-14 06:58:58 -0500

    added evidence gallery

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,14 @@ import Layout from './general/Layout';
 import ListComplaints from './general/ListComplaints';
 import MyComplaint from './general/MyComplaints';
 import PrimaryButton from './general/PrimaryButton';
+import EvidenceImageItem from './general/EvidenceImageItem';
 
-export {Complaints, Input, Layout, ListComplaints, MyComplaint, PrimaryButton};
+export {
+  Complaints,
+  Input,
+  Layout,
+  ListComplaints,
+  MyComplaint,
+  PrimaryButton,
+  EvidenceImageItem,
+};
--- src/containers/ComplaintDetails/layout/index.js
+++ src/containers/ComplaintDetails/layout/index.js
@@ -1,66 +1,131 @@
-import {Text, View, StyleSheet} from 'react-native';
-import {Layout, Input} from '../../../components';
+import {
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  Image,
+} from 'react-native';
+import {Layout, EvidenceImageItem} from '../../../components';
 import {colors} from '../../../library/styles/vars';
 
-import React from 'react';
+import React, {Component} from 'react';
 
-const ComplaintDetails = () => {
-  return (
-    <Layout backColor="DarkPrimary">
-      <View style={styles.container}>
-        <View style={styles.navigation} />
-        <View style={styles.infoSection}>
-          <View style={styles.element}>
-            <Text style={styles.tile}>Área: </Text>
-            <Text style={styles.description}>Seguridad</Text>
-          </View>
-          <View style={styles.separator} />
-          <View style={styles.element}>
-            <Text style={styles.tile}>Registro: </Text>
-            <Text style={styles.description}>06/05/21</Text>
-          </View>
-          <View style={styles.separator} />
-          <View style={styles.element}>
-            <Text style={styles.tile}>Ocurrido: </Text>
-            <Text style={styles.description}>06/05/21</Text>
-          </View>
-          <View style={styles.separator} />
-          <View style={styles.doubleElement}>
-            <Text style={styles.tile}>Ubicación: </Text>
-            <Text style={styles.description}>
-              Jalisco, Tlatomulco de Zúñiga, 45879, Hacienda Los Fresnos, Av.
-              Los Fresnos #345
-            </Text>
-          </View>
-          <View style={styles.separator} />
-          <View style={styles.doubleElement}>
-            <Text style={styles.tile}>Descripción: </Text>
-            <Text style={styles.description}>
-              En la colonia existe maltrano animal por parte de un becino
-              ubicado en la calle prados #435, los mantiene encerrados y sin
-              alimento
-            </Text>
-          </View>
-          <View style={styles.separator} />
+import ImageView from 'react-native-image-viewing';
+
+const images = [
+  {
+    id: 0,
+    uri:
+      'https://cdn.pixabay.com/photo/2017/08/17/10/47/paris-2650808_960_720.jpg',
+  },
+  {
+    id: 1,
+    uri:
+      'https://cdn.pixabay.com/photo/2021/02/23/14/15/dog-6043585_960_720.jpg',
+  },
+  {
+    id: 2,
+    uri:
+      'https://cdn.pixabay.com/photo/2015/03/26/09/54/pug-690566_960_720.jpg',
+  },
+  {
+    id: 3,
+    uri:
+      'https://cdn.pixabay.com/photo/2016/10/31/14/55/rottweiler-1785760_960_720.jpg',
+  },
+];
+
+class ComplaintDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {visible: true};
+  }
+  render() {
+    return (
+      <Layout backColor="DarkPrimary">
+        <View style={styles.container}>
+          <View style={styles.navigation} />
+          {this.infoSection()}
+          {this.evidenceGallery()}
+          <ImageView
+            images={images}
+            imageIndex={0}
+            visible={this.state.visible}
+            onRequestClose={() => this.setState({visible: false})}
+          />
         </View>
-        <View style={styles.infoSection2} />
+      </Layout>
+    );
+  }
+
+  evidenceGallery() {
+    return (
+      <SafeAreaView style={styles.infoSection}>
+        <View style={styles.element}>
+          <Text style={styles.tile}>Evidencia:</Text>
+        </View>
+        <FlatList
+          data={images}
+          renderItem={({item}) => <EvidenceImageItem uri={item.uri} />}
+          keyExtractor={item => item.id}
+          horizontal={true}
+        />
+      </SafeAreaView>
+    );
+  }
+
+  infoSection() {
+    return (
+      <View style={styles.infoSection}>
+        <View style={styles.element}>
+          <Text style={styles.tile}>Área: </Text>
+          <Text style={styles.description}>Seguridad</Text>
+        </View>
+        <View style={styles.separator} />
+        <View style={styles.element}>
+          <Text style={styles.tile}>Registro: </Text>
+          <Text style={styles.description}>06/05/21</Text>
+        </View>
+        <View style={styles.separator} />
+        <View style={styles.element}>
+          <Text style={styles.tile}>Ocurrido: </Text>
+          <Text style={styles.description}>06/05/21</Text>
+        </View>
+        <View style={styles.separator} />
+        <View style={styles.doubleElement}>
+          <Text style={styles.tile}>Ubicación: </Text>
+          <Text style={styles.description}>
+            Jalisco, Tlatomulco de Zúñiga, 45879, Hacienda Los Fresnos, Av. Los
+            Fresnos #345
+          </Text>
+        </View>
+        <View style={styles.separator} />
+        <View style={styles.doubleElement}>
+          <Text style={styles.tile}>Descripción: </Text>
+          <Text style={styles.description}>
+            En la colonia existe maltrano animal por parte de un vecino ubicado
+            en la calle prados #435, los mantiene encerrados y sin alimento
+          </Text>
+        </View>
+        <View style={styles.separator} />
       </View>
-    </Layout>
-  );
-};
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors.DiverColor,
+    backgroundColor: '#ffff',
     flex: 1,
     width: '100%',
   },
   navigation: {
     flex: 1,
+    backgroundColor: colors.LightPrimary,
   },
   infoSection: {
     flex: 6,
-    backgroundColor: 'orange',
     paddingHorizontal: 15,
     justifyContent: 'space-around',
   },
@@ -90,6 +155,11 @@ const styles = StyleSheet.create({
   },
   separator: {
     borderBottomWidth: 1,
+  },
+  image: {
+    height: 250,
+    width: 250,
+    backgroundColor: 'orange',
   },
 });
 
