3d6301e1dd4d39bba4c74cd16a9233947752cd0f
Coreandrum1 <47857455+Coreandrum1@users.noreply.github.com>
2021-05-20 09:36:03 -0500

    separated normalize functions to a method file

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import {Login, SignUp, MyComplaints} from './src/containers';
 
 import React from 'react';
-import {View} from 'react-native';
 import {useGoogleConfiguration} from './src/library/hooks';
 import {withRedux} from './src/library/redux';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+
 const Stack = createStackNavigator();
 
 const App = () => {
--- src/library/methods/index.js
+++ src/library/methods/index.js
@@ -1,10 +1,12 @@
 import {performGoogleAuth, performGoogleAuthSign} from './performAuth';
 import {sortArrayByDate} from './sortArrayByDate';
 import {dateFormatter} from './dateFormatter';
+import normalize from '../methods/normalize';
 
 export {
   performGoogleAuth,
   performGoogleAuthSign,
   sortArrayByDate,
   dateFormatter,
+  normalize,
 };
