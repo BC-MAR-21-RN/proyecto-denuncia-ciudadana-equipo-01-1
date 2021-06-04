5ce75c27961e2b7c31e64a63d0b6a90d8afe181c
jkzasori <jotafu@live.com>
2021-06-04 01:12:47 -0500

    Refactor: Creating only one ScreenContainer and tabNavigator

--- src/navigators/screen.container.js
+++ src/navigators/screen.container.js
@@ -8,7 +8,7 @@ import {
   MyComplaints,
   TitleDescriptionAdd,
 } from '../containers';
-
+import {Button} from 'react-native';
 import {Icon} from 'react-native-eva-icons';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
@@ -16,32 +16,73 @@ import TabNavigator from './tabs';
 import {createStackNavigator} from '@react-navigation/stack';
 
 export const Stack = createStackNavigator();
-const optionsBack = navigation => ({
+const optionsBack = (navigation, HLeft, HRight, color) => ({
   headerTitle: '',
   headerStyle: {
+    backgroundColor: color ? color : '#B9D7F2',
     elevation: 0, // remove shadow on Android
     shadowOpacity: 0, // remove shadow on iOS
   },
-  headerLeft: () => (
-    <Icon
-      name="arrow-back-outline"
-      width={48}
-      height={48}
-      fill="#3366FF"
-      onPress={() => navigation.goBack()}
-    />
-  ),
+  headerLeft: () => {
+    if (HLeft) {
+      return (
+        <Icon
+          name="arrow-back-outline"
+          width={48}
+          height={48}
+          fill="#3366FF"
+          onPress={() => navigation.goBack()}
+        />
+      );
+    }
+  },
+  headerRight: () => {
+    if (HRight) {
+      return (
+        <Button
+          onPress={() => {}}
+          title="🧑"
+          color={color ? color : '#B9D7F2'}
+        />
+      );
+    }
+  },
 });
 
 const listScreens = [
-  {name: 'Login', component: Login, back: false},
-  {name: 'Home', component: TabNavigator, back: false},
-  {name: 'MyComplaints', component: MyComplaints, back: true},
-  {name: 'TitleDescriptionAdd', component: TitleDescriptionAdd, back: true},
-  {name: 'DateEventsAdd', component: DateEventsAdd, back: true},
-  {name: 'AreaAdd', component: AreaAdd, back: true},
-  {name: 'LocationAdd', component: LocationAdd, back: true},
-  {name: 'EvidenceAdd', component: EvidenceAdd, back: true},
+  {name: 'Login', component: Login, headerLeft: false, headerRight: false},
+  {name: 'Home', component: TabNavigator, headerLeft: false, headerRight: true},
+  {
+    name: 'MyComplaints',
+    component: MyComplaints,
+    headerLeft: false,
+    headerRight: true,
+  },
+  {
+    name: 'TitleDescriptionAdd',
+    component: TitleDescriptionAdd,
+    headerLeft: false,
+    headerRight: true,
+  },
+  {
+    name: 'DateEventsAdd',
+    component: DateEventsAdd,
+    headerLeft: true,
+    headerRight: false,
+  },
+  {name: 'AreaAdd', component: AreaAdd, headerLeft: true, headerRight: false},
+  {
+    name: 'LocationAdd',
+    component: LocationAdd,
+    headerLeft: true,
+    headerRight: false,
+  },
+  {
+    name: 'EvidenceAdd',
+    component: EvidenceAdd,
+    headerLeft: true,
+    headerRight: false,
+  },
 ];
 
 export const Screens = () => {
@@ -52,8 +93,14 @@ export const Screens = () => {
           key={item.name}
           name={item.name}
           options={
-            item.back
-              ? ({navigation}) => optionsBack(navigation)
+            item.headerLeft || item.headerRight
+              ? ({navigation}) =>
+                  optionsBack(
+                    navigation,
+                    item.headerLeft,
+                    item.headerRight,
+                    item.color ? item.color : null,
+                  )
               : {headerShown: false}
           }
           component={item.component}
