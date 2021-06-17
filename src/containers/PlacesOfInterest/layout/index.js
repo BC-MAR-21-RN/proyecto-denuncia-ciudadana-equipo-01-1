198690249b84601babea6feaa705104f010ca068
Orlando Álvarez <wiki97ap@gmail.com>
2021-06-17 10:42:12 -0500

    rebase

--- src/containers/PlacesOfInterest/layout/index.js
+++ src/containers/PlacesOfInterest/layout/index.js
@@ -5,8 +5,9 @@ import { Text, View } from 'react-native'
 export default function PlacesOfInterest() {
     return (
         <View>
-            <Text>Holiwis</Text>
+           <View>
+
+           </View>
         </View>
     )
 }
-
