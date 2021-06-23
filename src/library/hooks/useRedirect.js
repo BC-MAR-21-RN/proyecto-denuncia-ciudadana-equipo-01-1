33fc571d61e3833faf0d4a3a59035185d1d1dcc7
Demis Alberto Rincón Martínez <darmfma@gmail.com>
2021-06-23 13:47:36 -0500

    changes

--- src/library/hooks/useRedirect.js
+++ src/library/hooks/useRedirect.js
@@ -6,12 +6,12 @@ export const useRedirect = props => {
   useEffect(() => {
     const timeout = setTimeout(() => {
       props.cleanAuth();
-      if (auth()?.currentUser?.uid) {
+      if (auth()?.currentUser?.uid&&auth().onAuthStateChanged()) {
         props?.navigation?.dispatch(StackActions.replace('Home'));
       }
     }, 1000);
     return () => {
       clearTimeout(timeout);
     };
-  }, [auth().currentUser, props?.navigation, props.error]);
+  }, [auth().currentUser, props?.navigation, props.error,auth().onAuthStateChanged()]);
 };
